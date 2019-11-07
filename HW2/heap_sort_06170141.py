610949624d8265b41a7e918e5977459da5d1965e
tzuying0312 <55644317+tzuying0312@users.noreply.github.com>
2019-11-07 14:45:32 +0800

    Update heap_sort_06170141.py

--- HW2/heap_sort_06170141.py
+++ HW2/heap_sort_06170141.py
@@ -1,4 +1,5 @@
 class Solution(object):
+    
     def heap_sort(mylist):
         for i in range(len(mylist),-1,-1): #從最後開始比較至第一位
             root=i #root為1
@@ -16,10 +17,10 @@ class Solution(object):
 
 
     def done (mylist):
-    
         ok.append(mylist[0]) #由於第一位元素已是最小的，因此append至ok的list中。
         mylist[0]=mylist[-1] #最後一位會成為新的一位，在此最小的list被取代成最後一位。
         mylist.pop(-1) #已將最後一元素一至第一位，因此刪除最後
+        
         print(ok) #查看每次ok中被丟入的元素。
     
         if len(mylist)>0:
