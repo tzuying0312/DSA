20dde0e946538b118dbdd80d5dc2cf4bd7b294a2
tzuying0312 <55644317+tzuying0312@users.noreply.github.com>
2019-11-07 14:54:56 +0800

    Update merge_sort_06170141.py

--- HW2/merge_sort_06170141.py
+++ HW2/merge_sort_06170141.py
@@ -1,25 +1,26 @@
 class Solution(object):
-    
+    #分割
     def spilt(list):    
-        if len(list)<2:
+        if len(list)<2:#list小於2，表示都已切至成單一個元素。
             return list
         else:
-            left=list[:len(list)//2]
+            left=list[:len(list)//2] #從長度的一半開始切，分成左右
             right=list[len(list)//2:]
-            return merge_sort(spilt(left),spilt(right))
+            return merge_sort(spilt(left),spilt(right)) #不斷切割至list小於2。
         
+    #合併
     def merge_sort(left,right):
-        i=0
-        j=0
+        i=0 #代表左邊元素的第幾個位置
+        j=0 #代表右邊元素的第幾個位置
         done=[]
-        while i<len(left) and j<len(right) :
+        while i<len(left) and j<len(right) : #左右兩邊的低一個元素開始比較
 
-            if left[i]>right[j]:
+            if left[i]>right[j]: #左大於右，將右邊的元素放置到done(排好)
                 done.append(right[j])
-                j+=1
+                j+=1 #由於右邊的第一位元素以排列完成，因此下一次比較的元素為右邊的第二位。
 
             else:
                 done.append(left[i])
                 i+=1
     
-        return done+left[i:]+right[j:]
+        return done+left[i:]+right[j:] #done+左與右的元素
