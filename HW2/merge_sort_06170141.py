0a445746bcb3959b2c94c43ab784faa54b1bb5ca
tzuying0312 <55644317+tzuying0312@users.noreply.github.com>
2019-11-10 09:39:22 +0800

    Update merge_sort_06170141.py

--- HW2/merge_sort_06170141.py
+++ HW2/merge_sort_06170141.py
@@ -1,15 +1,18 @@
 class Solution(object):
     #分割
-    def spilt(list):    
+    def merge_sort(self,list): 
+        self.list=list
         if len(list)<2:#list小於2，表示都已切至成單一個元素。
             return list
         else:
             left=list[:len(list)//2] #從長度的一半開始切，分成左右
             right=list[len(list)//2:]
-            return merge_sort(spilt(left),spilt(right)) #不斷切割至list小於2。
+        left=Solution().merge_sort(left)
+        right=Solution().merge_sort(right)
+        return Solution().merge(left,right) #不斷切割至list小於2。
         
     #合併
-    def merge_sort(left,right):
+    def merge(self,left,right):
         i=0 #代表左邊元素的第幾個位置
         j=0 #代表右邊元素的第幾個位置
         done=[]
